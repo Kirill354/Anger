710465ee2cae51c0a4301f984cd6087ebdc49633
Angry_Programist <Kirill_071@bk.ru>
2018-11-02 00:14:10 +0300

    commit

---
+++ PROJECTS/Test1/Summa.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Summa.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+
+
+	int main() {
+    int a;
+	int b;
+	cin>>a>>b;
+	if (a<10000)&&(b<10000);{
+	cout<<"a+b="<<a+b<<endl;
+	}
+	else {
+		cout<<"error"<<endl
+	}
+
+	}
+
+
+
+
+
+
