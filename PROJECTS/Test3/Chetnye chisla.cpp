20a0735cf426106a0820a98a91d4c3307c419502
Angry_Programist <Kirill_071@bk.ru>
2018-11-03 00:42:51 +0300

    commit

---
+++ PROJECTS/Test3/Chetnye chisla.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : Chetnye.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+//Дано два целых числа A и B (A <= B, A >= 1, B <= 30000).
+//Выведите через пробел все чётные числа от A до B (включительно).
+#include <iostream>
+using namespace std;
+
+ int main(){
+	int a;
+	int b;
+	if (a>b)/(a<1)/(b>30000) {
+		cout<<:"error"<<endl;
+	}
+	else {
+		vector <int>c:={a...b}
+		for (int d:c) {
+			if d%2=0 {
+				cout<<d<<endl;
+			}
+				else {
+					cout<<" "<<endl;
+				}
+
+			}
+		}
+	}
+
